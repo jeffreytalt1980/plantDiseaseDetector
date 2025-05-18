fc2a2f24d89e33f0bc8db7fc35e03a219ee149bd
Jeffrey Alt <jeffreytalt42@gmail.com>
2025-05-18 19:40:41 -0400

    added attention map display

--- app/page_files/predict_page.py
+++ app/page_files/predict_page.py
@@ -4,43 +4,77 @@ sys.modules['torch.classes'] = None # Exclude to prevent PyTorch/Streamlit compa
 import streamlit as st
 import torch
 import os
+import matplotlib.pyplot as plt
 
-from utils import load_model, preprocess_image, predict
+from utils import load_model, preprocess_image, predict, get_device, attention_maps
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = get_device()
 
 # Pytorch Model Prediction
-def on_predict_click(image: str):
+def on_predict_click(image_file: str):
     # Load the pre-trained model
     model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../swin_model_plant_disease_detector.pth"))
     model = load_model(model_path, device=device)
     
     # Preprocess the image
-    image_tensor = preprocess_image(image)
+    image_tensor = preprocess_image(image_file)
 
     # Predict the class
     return predict(model=model, image_tensor=image_tensor, device=device)
 
+def display_attention_map(image_file: str):
+    """
+    Display an attention map for Swin Transformer
+    """
+    st.header("Attention Maps:")
+
+    # Preprocess image
+    image_tensor = preprocess_image(image_file)
+    image = image_tensor.squeeze().permute(1, 2, 0).cpu().numpy()
+    image = (image * 0.5 + 0.5).clip(0, 1)  
+
+    fig, axes = plt.subplots(4, 3, figsize=(12, 16)) 
+    axes = axes.flatten()
+
+    for idx, attention_block in enumerate(attention_maps):
+        ax = axes[idx]
+        # reshape to remove the batch dimension
+        attention_vector = attention_block[0].mean(0).mean(0)
+        number_of_patches = attention_vector.shape[0]
+
+        height = width = int(number_of_patches ** 0.5)
+
+        # build the attention map
+        attention_map = attention_vector.reshape(height, width).detach().cpu().numpy()
+        attention_map = torch.nn.functional.interpolate(
+            torch.tensor(attention_map)[None, None], size=image.shape[:2], mode='bilinear'
+        )[0, 0].numpy()
+
+        ax.imshow(image)
+        ax.imshow(attention_map, alpha=0.5, cmap='jet')
+        ax.set_title(f"Block {idx}", fontsize=8)
+        ax.axis('off')
+
+    st.pyplot(fig) 
+    plt.close(fig)
+    
 def predict_page():
     st.header("Predict")
-    st.markdown(
-        """
-        Upload image files (png, jpg, or jpeg) of plant leaves to predict their condition.
-        """
-    )
     
     # File uploader
-    test_images = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+    test_images = st.file_uploader("Upload image files (png, jpg, or jpeg) of plant leaves to predict their condition.", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
     
-    if len(test_images) is 1:
-        st.image(test_images[0], caption='Uploaded Image', use_container_width=True)
-       
+    if len(test_images) == 1:
+        st.image(test_images[0], use_container_width=True)
+
         if(st.button("Predict")):
             with st.spinner("Thinking..."):
                 # Call the predict function
                 prediction, confidence = on_predict_click(test_images[0])
-                st.write("Prediction:")
+                st.header("Prediction:")
                 st.success(f"The model predicts that this is \"{prediction}\" with a confidence of {confidence*100:.2f}%.")
+                display_attention_map(test_images[0])
+
     elif len(test_images) > 1:
         if(st.button("Predictions")):
             with st.spinner("Thinking..."):
--- app/utils.py
+++ app/utils.py
@@ -9,6 +9,8 @@ from PIL import Image
 from constants import IMAGE_SIZE, NORMALIZATION_MEAN, NORMALIZATION_STD, class_names, class_names_output_map
 from model.CustomHead import CustomHead
 
+attention_maps = []
+
 def load_model(path: str, device: torch.device) -> torch.nn.Module:
     """
     Load the pre-trained model
@@ -26,6 +28,11 @@ def load_model(path: str, device: torch.device) -> torch.nn.Module:
     
     model.eval()
     model.to(device)
+
+    # Register the attention hook for each block
+    for layer in model.layers:
+        for block in layer.blocks:
+            block.attn.attn_drop.register_forward_hook(get_attention_hook)
     
     return model
     
@@ -53,13 +60,23 @@ def predict(model: torch.nn.Module, image_tensor: torch.Tensor, device: torch.de
     """
     Make a prediction using the model
     """
+    attention_maps.clear()
     image_tensor = image_tensor.to(device)
 
     # Make prediction
     with torch.no_grad():  
         prediction_weights = model(image_tensor).mean(dim=1)
-    
+
     prediction = torch.argmax(prediction_weights, dim=1).item()
     confidence = torch.softmax(prediction_weights, dim=1).max().item()
 
     return get_prediction_label(prediction), confidence
+
+def get_attention_hook(module, input, output):
+    """
+    Hook to get the attention weights
+    """
+    return attention_maps.append(output)
+
+def get_device() -> str:
+    torch.device("cuda" if torch.cuda.is_available() else "cpu")
